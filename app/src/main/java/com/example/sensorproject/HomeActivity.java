0a5200bf6f9ac88ca9fac312f41d9738d8b34504
nato93 <44198423+nato93@users.noreply.github.com>
2019-11-27 16:44:32 +0100

    changing value of database user

--- app/src/main/java/com/example/sensorproject/HomeActivity.java
+++ app/src/main/java/com/example/sensorproject/HomeActivity.java
@@ -1,5 +1,53 @@
 package com.example.sensorproject;
 
-public class HomeActivity {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class HomeActivity extends AppCompatActivity {
+
+    private TextView CoInput, TemperatureInput, HumidityInput;
+    DatabaseReference mRef;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        CoInput = findViewById(R.id.mCoInput);
+        TemperatureInput = findViewById(R.id.mTempInput);
+        HumidityInput = findViewById(R.id.mHumidInput);
+
+
+
+
+        //mRef = new FirebaseDatabase("https://sensorproject-876ad.firebaseio.com/");
+
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("Users");
+
+        myRef.setValue("Hello, World!");
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
