26c90c66a17b5022613e8b1da41145c836928251
nato93 <44198423+nato93@users.noreply.github.com>
2019-12-01 16:20:33 +0100

    retrieving data on the mainpageactivity

---
+++ app/src/main/java/com/example/sensorproject/MainPageActivity.java
@@ -0,0 +1,70 @@
+package com.example.sensorproject;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainPageActivity extends AppCompatActivity {
+    private TextView CoInput, TemperatureInput, HumidityInput;
+    private Button showButton;
+    DatabaseReference reff;
+
+
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+
+
+        CoInput = findViewById(R.id.mCoInput);
+        TemperatureInput = findViewById(R.id.mTempInput);
+        HumidityInput = findViewById(R.id.mHumidInput);
+        showButton = findViewById(R.id.mShowButton);
+
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reff = FirebaseDatabase.getInstance().getReference().child("users").child("user_one");
+                reff.addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        String co=dataSnapshot.child("co").getValue().toString();
+                        String temperature=dataSnapshot.child("temperature").getValue().toString();
+                        String humidity=dataSnapshot.child("humidity").getValue().toString();
+
+                        CoInput.setText(co);
+                        TemperatureInput.setText(temperature);
+                        HumidityInput.setText(humidity);
+                        //   mBook.setText(books);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+            }
+        });
+
+
+
+
+
+    }
+
+
+
+
+}
