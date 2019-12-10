66652cf89f91dd93f643e8034768185c38c1a786
nato93 <44198423+nato93@users.noreply.github.com>
2019-12-10 21:08:07 +0100

    changed the code from mainpageactivity to mainpagefragment

--- app/src/main/java/com/example/sensorproject/MainPageActivity.java
+++ app/src/main/java/com/example/sensorproject/MainPageActivity.java
@@ -24,7 +24,6 @@ import androidx.fragment.app.FragmentTransaction;
 
 public class MainPageActivity extends AppCompatActivity {
     private TextView RoomView;
-    DatabaseReference reff;
 
 
 
@@ -53,23 +52,6 @@ public class MainPageActivity extends AppCompatActivity {
 
 
 
-        // the gauge
-        final GaugeView mGv_co = findViewById(R.id.gv_co);
-        final GaugeView mGv_temp = findViewById(R.id.gv_humid);
-        final GaugeView mGv_humid = findViewById(R.id.gv_temp);
-
-
-        mGv_co.setShowRangeValues(true);
-        mGv_co.setTargetValue(0);
-
-        mGv_humid.setShowRangeValues(true);
-        mGv_humid.setTargetValue(0);
-
-        mGv_temp.setShowRangeValues(true);
-        mGv_temp.setTargetValue(0);
-
-
-
 
 /*
         CountDownTimer timer = new CountDownTimer(10000, 2) {
@@ -86,92 +68,8 @@ public class MainPageActivity extends AppCompatActivity {
         timer.start();
 */
 
-        reff = FirebaseDatabase.getInstance().getReference().child("users").child("user_one");
-        reff.addValueEventListener(new ValueEventListener() {
-
-            @Override
-            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-
-                try {
-                    String co = dataSnapshot.child("co").getValue().toString();
-                    String temperature = dataSnapshot.child("temperature").getValue().toString();
-                    String humidity = dataSnapshot.child("humidity").getValue().toString();
-                    String room = dataSnapshot.child("room").getValue().toString();
-                    mGv_co.setTargetValue(Float.parseFloat(co));
-                    mGv_temp.setTargetValue(Float.parseFloat(temperature));
-                    mGv_humid.setTargetValue(Float.parseFloat(humidity));
-                    //CoInput.setText(co);
-                    RoomView.setText(room);
 
 
-                }
-                catch (NullPointerException e){
-                    System.out.print("NullPointerException caught");
-                }
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-            }
-
-        });
-
-
-//CLICK LISTENERS
-
-        mGv_co.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(MainPageActivity.this, "ITS WORKING", Toast.LENGTH_SHORT).show();
-
-
-                //the content is currently inside the container: container_main
-                // when the CO gauge is clicked i want it to open the fragment: GaugeClickedFragment
-                // with the layout: container_gauge
-
-/*
-                FragmentManager fm = getSupportFragmentManager();
-                Fragment fragment = fm.findFragmentById(R.id.container_gauge);
-                fm.beginTransaction().add(R.id.container_gauge, new GaugeClickedFragment()).commit();
-
-
-                FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-                ft.replace(R.id.container_main, new GaugeClickedFragment());
-                ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN);
-                ft.commit();*/
-
-
-                // making the fragment hhh
-
-                GaugeClickedFragment gaugeClickedFragment = new GaugeClickedFragment();
-                FragmentManager fm = getSupportFragmentManager();
-
-                fm.beginTransaction().replace(R.id.container_gauge, gaugeClickedFragment).commit();
-
-
-            }
-        });
-
-        mGv_temp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(MainPageActivity.this, "ITS WORKING", Toast.LENGTH_SHORT).show();
-
-            }
-        });
-
-
-        mGv_humid.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                Toast.makeText(MainPageActivity.this, "ITS WORKING", Toast.LENGTH_SHORT).show();
-
-            }
-        });
 
 
     }//on create end
